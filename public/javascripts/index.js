66a08ee2d14fddc97b1cc4bb72b8992a0bcab8bb
Freya <18810375520@163.com>
2017-03-14 14:53:20 +0800

    fix javascript problem

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,16 @@
-console.log('test');
+//发布
+$('.btn-publish').on('click', function(){
+	console.log('555');
+	var blogContent = $('.textarea-add').val();
+	if(!blogContent){
+		alert('内容不能为空')
+	}
+	$.ajax({
+		type: "GET",
+		data: blogContent,
+		dataType: "json",
+		success: function(data){
+
+		}
+	})
+})
